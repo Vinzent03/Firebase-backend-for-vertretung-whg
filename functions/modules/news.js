dd72126ce3c57c30a12ed4dc74abcf7c60b83c50
Vinzent <vinzentwitte@gmail.com>
2020-10-26 15:22:59 +0100

    fix news notification and add last edited to news

--- functions/modules/news.js
+++ functions/modules/news.js
@@ -1,12 +1,19 @@
 const admin = require("firebase-admin");
 const db = admin.firestore();
 
+
+function getDateString() {
+    let date = new Date()
+    return dateString = date.getDate() + "." + (date.getMonth() + 1) + "." + date.getFullYear()
+}
+
+
 module.exports.addNews = async function (data, context) {
     let user = await admin.auth().getUser(context.auth.uid);
     if (user.customClaims['admin']) {
         let snap = await db.collection("news").doc("news").get()
         let news = snap.data().news
-        news.splice(0, 0, data.newNews)
+        news.splice(0, 0, { "title": data.newNews["title"], "text": data.newNews["text"], "lastEdited": getDateString() })
         let res = await db.collection("news").doc("news").set({ "news": news })
 
         var message = {
@@ -15,11 +22,12 @@ module.exports.addNews = async function (data, context) {
                 body: "Es gibt eine neue Nachricht, die vielleicht wichtig ist.",
             },
         };
-        await admin.messaging().send(message);
+        if (data.sendNotification)
+            await admin.messaging().sendToTopic("all", message);
         return { "code": "SUCCESS", "message": "Nachricht wurde gesendet" }
 
     } else {
-        return { "code": "ERROR_NOT_ADMIN", "message": "Du bist kein Admin, bitte melde dich ab und dann wieder an. Wenn du denktst du solltest Admin sein, melde dich bitte bei mir." }
+        return { "code": "ERROR_NOT_ADMIN", "message": "Du bist kein Admin! Eigentlich solltest du gar nicht hierhin kommen!" }
     }
 }
 module.exports.deleteNews = async function (data, context) {
@@ -40,8 +48,18 @@ module.exports.editNews = async function (data, context) {
     if (user.customClaims['admin']) {
         let snap = await db.collection("news").doc("news").get()
         let news = snap.data().news
-        news.splice(data.index, 1, data.newNews)
+        news.splice(data.index, 1, { "title": data.newNews["title"], "text": data.newNews["text"], "lastEdited": getDateString() })
         let res = await db.collection("news").doc("news").set({ "news": news })
+
+        var message = {
+            notification: {
+                title: "Eine Nachricht wurde bearbeitet",
+                body: "Diese Änderungen könnten wichtig sein.",
+            },
+        };
+        if (data.sendNotification)
+            await admin.messaging().sendToTopic("all", message);
+        
         return { "code": "SUCCESS", "message": "Nachricht wurde gesendet" }
 
     } else {
